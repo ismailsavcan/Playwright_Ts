3590f2bfc3bcb9d38e7a3a791262037039d092ad
Ismail Savcan <ismail.savcan@motion-software.com>
2023-12-31 17:57:34 +0300

    baseURL changed

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,7 +23,7 @@ export default defineConfig({
   reporter: "allure-playwright",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless:false,
+    headless:true,
     //baseURL:'https://qacart-todo.herokuapp.com',
     baseURL:'https://todo.qacart.com',
     /* Base URL to use in actions like `await page.goto('/')`. */
